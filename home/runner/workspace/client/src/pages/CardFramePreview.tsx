9f5c043b79b92e69dcb632a414d75f84ae2486ef
Dhenz14 <null>
2025-04-17 23:01:32 +0000

    Add utility files, store implementations, and card-related components

---
+++ home/runner/workspace/client/src/pages/CardFramePreview.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import CardFrameDemo from '../components/CardFrameDemo';
+
+/**
+ * Page component that renders the card frame demo
+ */
+const CardFramePreview: React.FC = () => {
+  return <CardFrameDemo />;
+};
+
+export default CardFramePreview;
